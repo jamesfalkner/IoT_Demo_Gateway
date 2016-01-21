2697d0fdb927ba450e53cb09e4b8ae14884901c7
Patrick Steiner <patrick@steiner-buchholz.de>
2016-01-21 15:37:02 +0100

    updates

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ export initialValue="70"
 export count="2"
 export waitTime="1"
 
-java -DdeviceType=$deviceType -DdeviceID=$deviceID -DinitialValue=$initialValue -Dcount=$count -DwaitTime=$waitTime -jar producer/target/iot_producer-jar-with-dependencies.jar
+java -DdeviceType=$deviceType -DdeviceID=$deviceID -DinitialValue=$initialValue -Dcount=$count -DwaitTime=$waitTime -jar Producer/target/iot_producer-jar-with-dependencies.jar
