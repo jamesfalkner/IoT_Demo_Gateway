2409bdb6ce3dcd2ee76729b987595c7ea1545dd9
Patrick Steiner <psteiner@Patricks-MBP.fritz.box>
2016-02-11 20:35:15 +0100

    initial version on OSGi

--- FuseEAP/Docker_Files/scripts/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "copy deployable to EAP deployment folder"
-
-cp /home/psteiner/deployable/smart_gateway.war /home/psteiner/jboss/jboss-eap-6.4/standalone/deployments
-
-/home/psteiner/jboss/jboss-eap-6.4/bin/standalone.sh -c standalone-full.xml
