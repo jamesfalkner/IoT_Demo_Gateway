818bfaf84a4d2e56656af8a54bce6507b5336ae7
Patrick Steiner <psteiner@Patricks-MBP.fritz.box>
2016-02-16 16:18:03 +0100

    fixed environment variable issues

--- Rules_CEP/Docker_Files/scripts/run.sh
+++ Rules_CEP/Docker_Files/scripts/run.sh
@@ -3,5 +3,7 @@
 # Little helper for the installation of Red Hat JBoss Fuse in a Docker container
 #
 
+echo TARGET_AMQ_BROKET
+
 echo "Starting rules_cep"
 java -jar $HOME/target/$APPL
--- Rules_CEP/src/main/java/com/redhat/demo/iot/gateway/rules_cep/App.java
+++ Rules_CEP/src/main/java/com/redhat/demo/iot/gateway/rules_cep/App.java
@@ -1,42 +1,27 @@
 package com.redhat.demo.iot.gateway.rules_cep;
 
-import java.io.IOException;
 import java.io.StringReader;
-import java.io.StringWriter;
 import java.util.logging.Logger;
 
-import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
-import javax.jms.TextMessage;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
-
-import org.apache.activemq.ActiveMQConnectionFactory;
 
 public class App 
 {
     private static final Logger log = Logger.getLogger(CepServer.class.getName());
-	
-    private static final String DEFAULT_SOURCE_AMQ_BROKER	= "tcp://smartgateway:61616";
-    private static final String DEFAULT_TARGET_AMQ_BROKER	= "tcp://192.168.178.92:61616";
-    private static final String DEFAULT_SOURCE_QUEUE 		= "message.to.rules";
-    private static final String DEFAULT_TARGET_QUEUE		= "message.to.datacenter";
-    private static final String DEFAULT_BROKER_ADMIN_UID	= "admin";
-    private static final String DEFAULT_BROKER_ADMIN_PASSWD	= "change12_me";
-    
-		 
+			 
     public static void main( String[] args ) throws InterruptedException, JMSException, JAXBException
     {
     	String 	messageFromQueue;
     	
-    	String 	sourceAMQBroker = System.getProperty("SOURCE_AMQ_BROKER",DEFAULT_SOURCE_AMQ_BROKER);
-    	String 	targetAMQBroker = System.getProperty("TARGET_AMQ_BROKER",DEFAULT_TARGET_AMQ_BROKER);
-    	String 	sourceQueue 	= System.getProperty("SOURCE_QUEUE",DEFAULT_SOURCE_QUEUE);
-    	String 	targetQueue 	= System.getProperty("TARGET_QUEUE",DEFAULT_TARGET_QUEUE);
-    	String  brokerUID		= System.getProperty("BROKER_ADMIN_UID",DEFAULT_BROKER_ADMIN_UID);
-    	String  brokerPassword  = System.getProperty("BROKER_ADMIN_PASSWD",DEFAULT_BROKER_ADMIN_PASSWD);
+    	String 	sourceAMQBroker = System.getenv("SOURCE_AMQ_BROKER");
+    	String 	targetAMQBroker = System.getenv("TARGET_AMQ_BROKER");
+    	String 	sourceQueue 	= System.getenv("SOURCE_QUEUE");
+    	String 	targetQueue 	= System.getenv("TARGET_QUEUE");
+    	String  brokerUID		= System.getenv("BROKER_ADMIN_UID");
+    	String  brokerPassword  = System.getenv("BROKER_ADMIN_PASSWD");
 
         System.out.println("TARGET_AMQ_BROKER = " + targetAMQBroker);
     
