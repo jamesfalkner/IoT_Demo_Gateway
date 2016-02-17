e5a661ba98dd6aa330b54a2660c41427a3ba816f
Patrick Steiner <psteiner@Patricks-MBP.fritz.box>
2016-02-17 14:34:28 +0100

    first attempt to solve automatic session closure

--- Rules_CEP/src/main/java/com/redhat/demo/iot/gateway/rules_cep/Producer.java
+++ Rules_CEP/src/main/java/com/redhat/demo/iot/gateway/rules_cep/Producer.java
@@ -21,17 +21,35 @@ public class Producer {
 	 private Connection connection;
 	 private Session session;
 	 private MessageProducer producer;
+	 private String queueName	="";
+	 private String brokerURL	="";
+	 private String uid			= "";
+	 private String passwd		= "";
  
 	 public Producer(String queueName, String brokerURL, String uid, String passwd) throws JMSException
 	 {
+		this.queueName 	= queueName;
+		this.brokerURL 	= brokerURL;
+		this.uid		= uid;
+		this.passwd		= passwd;
+		 
+		connect(queueName, brokerURL, uid, passwd);
+	 }
+	 
+	 public void connect(String queueName, String brokerURL, String uid, String passwd) {
 		// setup the connection to ActiveMQ
-	    factory = new ActiveMQConnectionFactory(uid, passwd, brokerURL);
-	    	
-        connection = factory.createConnection();
-        connection.start();
-        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-        Destination destination = session.createQueue(queueName);
-        producer = session.createProducer(destination);
+		    factory = new ActiveMQConnectionFactory(uid, passwd, brokerURL);
+		    	
+	        try {
+				connection = factory.createConnection();
+				connection.start();
+		        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		        Destination destination = session.createQueue(queueName);
+		        producer = session.createProducer(destination);
+			} catch (JMSException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	 }
  
 	 public void run(DataSet data) {
@@ -54,7 +72,7 @@ public class Producer {
 	 
 	 public void run(String data)
 	 {
-    
+		
         Message message;
 		try {
 			message = session.createTextMessage( data );
@@ -62,11 +80,16 @@ public class Producer {
 			producer.send(message);
 		} catch (JMSException e) {
 			// TODO Auto-generated catch block
+			System.out.println("Session to broker closed due to no traffic, need to reconnect");
+			
+			connect(queueName, brokerURL, uid, passwd);
+			
 			e.printStackTrace();
 		}
    
 	 }
  
+	 
     public void close() throws JMSException
     {
         if (connection != null)
