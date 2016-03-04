8546feccc3debfcf5f68a17ccf89e29a32d52a42
Patrick Steiner <patrick@steiner-buchholz.de>
2016-03-04 14:52:43 +0100

    cleanup for Raspberry Version

--- Rules_CEP/Docker_Files/scripts/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Little helper for the installation of Red Hat JBoss Fuse in a Docker container
-#
-
-echo TARGET_AMQ_BROKET
-
-echo "Starting rules_cep"
-java -jar $HOME/target/$APPL
